572482ed3982221a6e22b9312b2a08ca5e60a494
FFF <fumi238.1447@gmail.com>
2026-05-07 23:02:01 +0900

    Create A.cpp

---
+++ ABC/ABC455/A.cpp
@@ -0,0 +1,21 @@
+/*
+問題名：A - 455
+結果：AC
+実行時間：1 ms / メモリ　3684 KiB
+メモ：かつ（&&）を用いてシンプルに判定。
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B, C;
+  cin >> A >> B >> C;
+  
+  if(A != B && B == C){
+    cout << "Yes" << endl;
+  } 
+  else{
+    cout << "No" << endl;
+  }
+}
