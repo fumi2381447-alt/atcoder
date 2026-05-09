97a1482b49c9a68ef7596fa9420c8756264e90d2
FFF <fumi238.1447@gmail.com>
2026-05-09 20:41:38 +0900

    Create A_Edge Checker_2.cpp

---
+++ ADT/0504_EASY/A_Edge Checker_2.cpp
@@ -0,0 +1,19 @@
+// 二分木でa,bが繋がっているか？
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int a, b;
+  cin >> a >> b;
+  
+  if(a > b) swap(a, b);
+  
+  if(b / 2 == a) {
+    cout << "Yes" << "\n";
+  } else {
+  cout << "No" << "\n";
+  }
+  return 0;
+}
